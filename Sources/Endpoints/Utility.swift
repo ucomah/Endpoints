0036cc6b583fd2c1713347b94e5fb32401c01d6c
Eugene <>
2021-09-06 11:44:36 +0300

    Endpoint protocol and Utilities.

---
+++ Sources/Endpoints/Utility.swift
@@ -0,0 +1,24 @@
+#if canImport(Foundation)
+import Foundation
+#endif
+
+extension Dictionary where Key: ExpressibleByStringLiteral, Value: Any {
+
+    func encodedFor(method: HTTP.Method) -> String? {
+        guard method == .get else { return nil }
+        var addons = [String]()
+        for (key, value) in self {
+            if let arr = value as? [String] {
+                addons.append(contentsOf: arr.map { "\(key)=\($0)" })
+            } else {
+                addons.append("\(key)=\(String(describing: value))")
+            }
+        }
+        guard !addons.isEmpty else { return nil }
+        let tmp = "?" + addons.joined(separator: "&")
+        if let result = tmp.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) {
+            return result
+        }
+        return nil
+    }
+}
