b9d28008a484de0329b34db8aa81dda991bfbb30
Eugene <>
2021-09-06 11:41:40 +0300

    Basic HTTP declarations.

---
+++ Sources/Endpoints/HTTP.swift
@@ -0,0 +1,18 @@
+public final class HTTP {
+    
+    private init() {}
+    
+    public enum Method: String {
+        case options = "OPTIONS"
+        case get     = "GET"
+        case head    = "HEAD"
+        case post    = "POST"
+        case put     = "PUT"
+        case patch   = "PATCH"
+        case delete  = "DELETE"
+        case trace   = "TRACE"
+        case connect = "CONNECT"
+    }
+
+    public typealias Headers = [String: String]
+}
